aa6171b235e228204acf70be7958faf6e35908fa
Muhamad Irham Prasetyo <irhamputraprasetyo@gmail.com>
2018-03-28 18:29:07 +0200

    downvoteTalk resolver

--- resolvers/resolvers.js
+++ resolvers/resolvers.js
@@ -35,10 +35,24 @@ export default {
             return talk
         },
 
-        deleteTalk: async (parent, { id }, { Talk }) => {
-            const talk = await Talk.remove(id);
+        downvoteTalk: async (parent, args, { Talk }) => {
+            const talk = await Talk.findById(args.id);
+
+            talk.set({
+                votes: talk.votes ? talk.votes -1 : -1
+            });
+
+            await talk.save();
+
+            return talk;
+        },
+
+        deleteTalk: async (parent, args, { Talk }) => {
+            const talk = await Talk.findById(args.id);
 
             talk._id = talk._id.toString();
+
+            await talk.remove();
 
             return talk
         }
